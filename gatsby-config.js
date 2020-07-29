ee86ac6033c219b5a3b19c12823e543e1ac64fc7
Derek <derek@insaite.io>
2020-07-28 21:48:58 -0500

    whatsapp floating icon added finally

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
     title: config.siteTitle,
   },
   plugins: [
-    'gatsby-plugin-fontawesome-css',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
--- src/components/Whats.js
+++ src/components/Whats.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import '../assets/sass/grayscale.scss';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
-
-library.add(fab, faCheckSquare, faWhatsapp, faCoffee)
 
 export default function Whats() {
   return (
     // <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <a href="https://api.whatsapp.com/send?phone=5573017740&text=Que%20Pedal%20Me%20llamo%20Derek." class="float" target="_blank">
-      
-      <FontAwesomeIcon icon={['fab', 'whatsapp']} />
+      <i className={'fab fa-whatsapp'}></i>
+    
           </a>
     );
   }
-export const Showcase = () => (
-  <div>
-    <FontAwesomeIcon icon={['fab', 'apple']} />
-    <FontAwesomeIcon icon={['fab', 'microsoft']} />
-    <FontAwesomeIcon icon={['fab', 'google']} />
-
-    <FontAwesomeIcon icon="check-square" />
-    With Coffee Checked, these companies always know their coffee is hot and ready!
-  </div>
-)
